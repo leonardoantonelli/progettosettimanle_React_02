22a6523968d910085685d2a258f7af3f185dd540
leonardoantonelli <antonellileonardodesign@gmail.com>
2023-02-17 16:14:37 +0100

    tutto su app js

---
+++ my-app/src/components/api.js
@@ -0,0 +1,9 @@
+export const GeoApi = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "45d4a6884bmshfac3de3dad532bbp182015jsnd3cafd3966e9",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GeoUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
